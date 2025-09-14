a7a0ada8668dc65da6885033028c72c9d0ecf3f5
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-09-14 15:27:39 +0900

    feat: imporove PrimaryButton reusable component

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+  function pressed() {
+    console.log("Pressed");
+  }
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressed}
+        android_ripple={{ color: "#644233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    padding: 8,
+    borderRadius: 4,
+    elevation: 2,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
